566284610aae741e07dcc97bad12178ea5fdf772
Fanboy Coder <joel.monteiro.ferreira@gmail.com>
2022-09-08 09:02:20 +0100

    Fixed the list error

--- src/display.js
+++ src/display.js
@@ -1,14 +1,15 @@
-import { addTodo } from "./new-todo";
+const list = [];
 
 function display () {
-    for (let i = 0; i< addTodo.list.length; i++) {
+
+    for (let i = 0; i< list.length; i++) {
         const container = document.getElementById("display");
         const div = container.appendChild(document.createElement("div"));
-        for (let elem in addTodo.list[i]) {
+        for (let elem in list[i]) {
             const p = div.appendChild(document.createElement("p"));
-            p.innerText = addTodo.list[i][elem];
+            p.innerText = list[i][elem];
         }
     }
 }
 
-export { display }
+export { display, list }
--- src/new-todo.js
+++ src/new-todo.js
@@ -1,7 +1,7 @@
 import { todo } from "./item";
+import { list } from "./display";
 
 function addTodo() {
-    const list = [];
     const submit = document.querySelector("#submit");
     submit.addEventListener("click", () => {
         let title = document.getElementById("new-title").value;
@@ -9,10 +9,8 @@ function addTodo() {
         let dueDate = document.getElementById("start").value;
         let priority = document.querySelector("input[name=priority]:checked").value;
         const newTodo = todo (title, description, dueDate, priority);
-        return newTodo;
+        list.push(newTodo);
     })
-    list.push(newTodo);
-    console.log(list);
 }
 
 export { addTodo }
